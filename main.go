a19f606891ca7f32cf47700737b6799f406fbac8
Ifeanyi batman <anyimifeanyi0@gmail.com>
2026-01-23 18:32:36 +0100

    feat: Add check server health endpoint.

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ func main(){
 	srv := http.Server {
 		Addr : ":8080" ,
 		Handler : serveMux }
-	serveMux.Handle("/",http.FileServer(http.Dir(".")))
+		
+	serveMux.Handle("/app/",http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz",healthz)
 	srv.ListenAndServe()
 }
+
+func healthz (w http.ResponseWriter,req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	
+	}
