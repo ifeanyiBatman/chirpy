a10425952e99f199431412ba284d2fdbb342d424
Ifeanyi batman <anyimifeanyi0@gmail.com>
2026-01-24 13:13:51 +0100

    feat: Add request counting middleware and metrics endpoints for file server hits with reset endpoint.

--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
-func main(){
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func main() {
+	apiCfg := &apiConfig{}
 
 	serveMux := http.NewServeMux()
-	srv := http.Server {
-		Addr : ":8080" ,
-		Handler : serveMux }
-		
-	serveMux.Handle("/app/",http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	serveMux.HandleFunc("/healthz",healthz)
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+
+	serveMux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("GET /healthz", healthz)
+	serveMux.HandleFunc("GET /metrics", apiCfg.metrics)
+	serveMux.HandleFunc("POST /reset",apiCfg.resetMetrics)
 	srv.ListenAndServe()
 }
 
-func healthz (w http.ResponseWriter,req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+func healthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig)  resetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
 	
-	}
+}
